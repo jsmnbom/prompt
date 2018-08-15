768af2b610e8bea1a613a08223d68554a5eef7e5
Jasmin Bom <bomjacob@gmail.com>
2018-08-15 13:11:35 +0200

    Opt out of service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import {HashRouter} from "react-router-dom";
 
 ReactDOM.render((
@@ -9,4 +9,4 @@ ReactDOM.render((
         <App/>
     </HashRouter>
 ), document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
