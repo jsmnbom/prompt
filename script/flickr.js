e715238af53ac7f4b8ff316131617debdd3de17c
Jasmin Bom <bomjacob@gmail.com>
2018-08-21 21:58:59 +0200

    Update flickr script

--- script/flickr.js
+++ script/flickr.js
@@ -4,6 +4,7 @@ const meow = require('meow');
 const util = require('util');
 const fs = require('fs');
 const pick = require("lodash/object").pick;
+const uniqBy = require("lodash/array").uniqBy;
 const pprint = (obj) => console.log(util.inspect(obj, {showHidden: false, depth: null}));
 
 const cli = meow(`
@@ -19,13 +20,13 @@ const licenses = [1/*by-nc-sa*/, 2/*by-nc*/, 3/*by-nc-nd*/, 4/*by*/, 5/*by-sa*/,
 let flickr = new Flickr(process.env.FLICKR_API_KEY);
 
 function getImages(query, min, cb) {
-    const images = [];
+    let images = [];
     let i = 0;
 
     function inner(page) {
         console.log(page, i);
         i++;
-        if (i > 3) {
+        if (i > 7) {
             process.exit()
         }
         flickr.photos.search({
@@ -34,7 +35,14 @@ function getImages(query, min, cb) {
         }).then(function (res) {
             res.body.photos.photo.forEach((photo) => {
                 if (photo.url_z || photo.url_b || photo.url_h) {
-                    images.push(pick(photo, ['id', 'owner', 'ownername', 'title', 'license', 'views', 'url_z', 'url_b', 'url_h']))
+                    const img = pick(photo, ['id', 'owner', 'ownername', 'title', 'license']);
+                    img.urlBase = photo.url_z.split('_')[0] + '_';
+                    img.url_m = photo.url_m.replace(img.urlBase, '');
+                    img.url_z = photo.url_z.replace(img.urlBase, '');
+                    if (photo.url_b) img.url_b = photo.url_b.replace(img.urlBase, '');
+                    if (photo.url_h) img.url_h = photo.url_h.replace(img.urlBase, '');
+                    images.push(img);
+                    images = uniqBy(images, 'id');
                 } else {
                     console.log(photo);
                 }
@@ -55,13 +63,13 @@ function getImages(query, min, cb) {
 getImages({
     text: text,
     license: licenses.join(','),
-    extras: 'license, owner_name, icon_server, views, url_z, url_b, url_h',
+    extras: 'license, owner_name, icon_server, views, url_m, url_z, url_b, url_h',
     sort: 'interestingness-desc'
-}, 100, (images) => {
+}, 500, (images) => {
     fs.writeFile(`../src/data/motifs/${name.toLowerCase()}.json`, JSON.stringify({
         images: images,
         name: name,
-        thumb: 0
+        thumb: null
     }), 'utf8');
 });
 
