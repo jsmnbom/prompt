53ca2efe66f675a1a0f621912e404958c0d38a07
Jasmin Bom <bomjacob@gmail.com>
2018-08-22 14:04:34 +0200

    Add bridge and people motifs

--- src/data/motifs/index.js
+++ src/data/motifs/index.js
@@ -1,18 +1,24 @@
 import birds from "./birds"
+import bridges from "./bridges"
 import cats from "./cats"
 import dogs from "./dogs"
 import insects from "./insects"
 import lizards from "./lizards"
+import people from "./people"
 import trees from "./trees"
+// import test from "./test"
 
 
 const categories = {
     Birds: birds,
+    Bridges: bridges,
     Cats: cats,
     Dogs: dogs,
     Insects: insects,
     Lizards: lizards,
-    Trees: trees
+    People: people,
+    Trees: trees,
+    // Test: test
 };
 
 export default Object.values(categories);
